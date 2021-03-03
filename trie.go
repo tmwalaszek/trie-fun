835778cb222575a0a1e0286be6fe1ec89c009a4f
Tomasz Walaszek <tmwalaszek@gmail.com>
2021-03-03 22:24:31 +0100

    Remove recursive functions

--- trie.go
+++ trie.go
@@ -37,36 +37,41 @@ func (t *Trie) FindKey(key string) (interface{}, error) {
 		return 0, ErrEmptyKey
 	}
 
-	return t.findKey(key, nil), nil
+	return t.findKey(key), nil
 }
 
-func (t *Trie) findKey(key string, node *TrieNode) interface{} {
-	if node == nil {
-		if t.Childrens == nil {
+func (t *Trie) findKey(key string) interface{} {
+	var node *TrieNode
+	var ok bool
+
+	for {
+		if node == nil {
+			if t.Childrens == nil {
+				return nil
+			}
+
+			node, ok = t.Childrens[rune(key[0])]
+			if !ok {
+				return nil
+			}
+
+			key = key[1:]
+			continue
+		}
+
+		if node.RemainingKey == key {
+			return node.Value
+		}
+
+		if node.Childrens == nil {
 			return nil
 		}
 
-		val, ok := t.Childrens[rune(key[0])]
+		node, ok = node.Childrens[rune(key[0])]
 		if !ok {
 			return nil
 		}
-
-		return t.findKey(key[1:], val)
-	}
-
-	if node.RemainingKey == key {
-		return node.Value
-	}
-
-	if node.Childrens == nil {
-		return nil
-	}
-
-	val, ok := node.Childrens[rune(key[0])]
-	if !ok {
-		return nil
-	} else {
-		return t.findKey(key[1:], val)
+		key = key[1:]
 	}
 }
 
@@ -78,86 +83,92 @@ func (t *Trie) AddValue(key string, value interface{}) (bool, error) {
 		return false, ErrEmptyKey
 	}
 
-	return t.addValue(key, value, nil), nil
+	return t.addValue(key, value), nil
 }
 
-func (t *Trie) addValue(key string, value interface{}, node *TrieNode) bool {
-	if node == nil {
-		val, ok := t.Childrens[rune(key[0])]
+func (t *Trie) addValue(key string, value interface{}) bool {
+	var node *TrieNode
+	var ok bool
+
+	for {
+		if node == nil {
+			node, ok = t.Childrens[rune(key[0])]
+			if !ok {
+				t.Childrens[rune(key[0])] = &TrieNode{
+					RemainingKey: key[1:],
+					Value:        value,
+				}
+
+				return false
+			}
+
+			key = key[1:]
+		}
+
+		var updated bool
+		if key == node.RemainingKey {
+			if node.Value != nil {
+				updated = true
+			}
+
+			node.Value = value
+			return updated
+		}
+
+		if key == "" && node.RemainingKey == "" {
+			if node.Value != nil {
+				updated = true
+			}
+
+			node.Value = value
+			return updated
+		}
+
+		if key == "" && node.RemainingKey != "" {
+			if node.Childrens == nil {
+				node.Childrens = make(map[rune]*TrieNode)
+			}
+
+			node.Childrens[rune(node.RemainingKey[0])] = &TrieNode{
+				RemainingKey: node.RemainingKey[1:],
+				Value:        node.Value,
+			}
+
+			node.RemainingKey = ""
+			node.Value = value
+			return updated
+		}
+
+		var newNode *TrieNode
+		if node.Childrens == nil {
+			node.Childrens = make(map[rune]*TrieNode)
+
+			node.Childrens[rune(node.RemainingKey[0])] = &TrieNode{
+				RemainingKey: node.RemainingKey[1:],
+				Value:        node.Value,
+			}
+
+			newNode = node.Childrens[rune(node.RemainingKey[0])]
+
+			node.RemainingKey = ""
+			node.Value = nil
+		}
+
+		if newNode == nil {
+			newNode = node.Childrens[rune(key[0])]
+		}
+
+		_, ok := node.Childrens[rune(key[0])]
 		if !ok {
-			t.Childrens[rune(key[0])] = &TrieNode{
+			node.Childrens[rune(key[0])] = &TrieNode{
 				RemainingKey: key[1:],
 				Value:        value,
 			}
+
 			return false
 		}
 
-		node = val
 		key = key[1:]
+		node = newNode
 	}
-
-	var updated bool
-	if key == node.RemainingKey {
-		if node.Value != nil {
-			updated = true
-		}
-
-		node.Value = value
-		return updated
-	}
-
-	if key == "" && node.RemainingKey == "" {
-		if node.Value != nil {
-			updated = true
-		}
-
-		node.Value = value
-		return updated
-	}
-
-	if key == "" && node.RemainingKey != "" {
-		if node.Childrens == nil {
-			node.Childrens = make(map[rune]*TrieNode)
-		}
-
-		node.Childrens[rune(node.RemainingKey[0])] = &TrieNode{
-			RemainingKey: node.RemainingKey[1:],
-			Value:        node.Value,
-		}
-
-		node.RemainingKey = ""
-		node.Value = value
-		return updated
-	}
-
-	var newNode *TrieNode
-	if node.Childrens == nil {
-		node.Childrens = make(map[rune]*TrieNode)
-
-		node.Childrens[rune(node.RemainingKey[0])] = &TrieNode{
-			RemainingKey: node.RemainingKey[1:],
-			Value:        node.Value,
-		}
-
-		newNode = node.Childrens[rune(node.RemainingKey[0])]
-
-		node.RemainingKey = ""
-		node.Value = nil
-	}
-
-	if newNode == nil {
-		newNode = node.Childrens[rune(key[0])]
-	}
-
-	_, ok := node.Childrens[rune(key[0])]
-	if !ok {
-		node.Childrens[rune(key[0])] = &TrieNode{
-			RemainingKey: key[1:],
-			Value:        value,
-		}
-
-		return false
-	}
-
-	return t.addValue(key[1:], value, newNode)
 }
